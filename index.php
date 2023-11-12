0d1560696fce0d54733aee9e525d373096788576
raffichaesa <140777811+RaffiChaesa@users.noreply.github.com>
2023-11-12 18:12:21 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Selamat Berwisata...</title>
+</head>
+
+<body>
+    <section class="landing">
+        <div>
+            <h1>Secukupnya...</h1>
+            <p>
+                Selamat berwisata di tempat di mana setiap sudut menyimpan serpihan kenangan masa lalu. Di
+                sini, kita tidak hanya merayakan kebahagiaan, tetapi juga mengenang melalui setiap jejak duka yang
+                pernah kita lewati. Berkelanalah melalui lorong waktu, temukan dan simpanlah kembali momen-momen indah
+                dan tantangan yang telah membentuk kita.
+            </p>
+
+            <button>Mulai...</button>
+        </div>
+        <div>
+            <img src="./images/secukupnya.jpg" alt="secukupnya" width="350px">
+        </div>
+    </section>
+</body>
+
+</html>
