476427f1d8edef643108869c4dbdefa14d12e7e4
raffichaesa <140777811+RaffiChaesa@users.noreply.github.com>
2023-11-14 14:25:32 +0700

    Update login.php

--- login.php
+++ login.php
@@ -10,10 +10,20 @@ if (isset($_POST['username'])) {
         $_SESSION['username'] = $username;
         header('location:home.php');
     } else {
-        $gagal = "gagal";
+        $gagal = true;
     }
 }
-if (isset ($_GET ["action"])) {
+if (isset($_POST['newusername'])) {
+    $newusername = $_POST['newusername'];
+    $newpassword = $_POST['newpassword'];
+    $query = mysqli_query($connect, "INSERT INTO user VALUES ('$newusername', '$newpassword')");
+    if ($query) {
+        header("location:login.php");
+    } else {
+        $gagal = true;
+    }
+}
+if (isset($_GET["action"])) {
     $signup = true;
 }
 ?>
@@ -29,20 +39,36 @@ if (isset ($_GET ["action"])) {
 
 <body>
     <section class="login">
-        <div class="login-left">
-            <h1>Mulai...</h1>
-            <?php if(isset($gagal)):?>
-                <p>Coba mulai lagi...</p>
-            <?php endif?>
-            <form action="login.php" method="POST">
-                <label for="username">Nama pengguna</label><br>
-                <input type="text" name="username" id="username"><br>
-                <label for="password">Kata sandi</label><br>
-                <input type="password" name="password" id="password">
-                <button type="submit">Berwisata...</button>
-            </form>
-            <a href="login.php?action=signup"><button type="button">Buat akun...</button></a>
-        </div>
+        <?php if (!isset($signup)): ?>
+            <div class="login-left">
+                <h1>Mulai...</h1>
+                <?php if (isset($gagal)): ?>
+                    <p>Coba mulai lagi...</p>
+                <?php endif ?>
+                <form action="login.php" method="POST">
+                    <label for="username">Nama pengguna</label><br>
+                    <input type="text" name="username" id="username"><br>
+                    <label for="password">Kata sandi</label><br>
+                    <input type="password" name="password" id="password">
+                    <button type="submit">Berwisata...</button>
+                </form>
+                <a href="login.php?action=signup"><button type="button">Buat akun...</button></a>
+            </div>
+        <?php else: ?>
+            <div class="login-left">
+                <h1>Buat akun...</h1>
+                <?php if (isset($gagal)): ?>
+                    <p>Coba mulai lagi...</p>
+                <?php endif ?>
+                <form action="login.php" method="POST">
+                    <label for="newusername">Nama pengguna</label><br>
+                    <input type="text" name="newusername" id="newusername"><br>
+                    <label for="newpassword">Kata sandi</label><br>
+                    <input type="password" name="newpassword" id="newpassword">
+                    <button type="submit">Buat...</button>
+                </form>
+            </div>
+        <?php endif ?>
         <div>
             <img src="./images/mulai.jpg" alt="mulai" width="350px">
         </div>
