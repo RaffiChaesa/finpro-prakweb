1604fcc338929e4377bc2ec8e74d728d3fadd884
Cavaboy <110608653+Cavaboy@users.noreply.github.com>
2023-11-28 08:40:59 +0700

    styling

--- detail.php
+++ detail.php
@@ -23,7 +23,8 @@ if (isset($_GET["action"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/detail.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Selamat berwisata...</title>
 </head>
 
